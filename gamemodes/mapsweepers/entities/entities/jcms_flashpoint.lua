5bf68ed00976380f7168f3266f5ff239f355d525
JonahSoldier <86782614+JonahSoldier@users.noreply.github.com>
2025-07-28 11:30:55 +0100

    Fix bad order of operations
    Exponentiation was running before the length of the table was grabbed, causing an error.
    
    This was my mistake. This is why you always test things. I think I slipped up because I got distracted with bspreader stuff and error logging.

--- gamemodes/mapsweepers/entities/entities/jcms_flashpoint.lua
+++ gamemodes/mapsweepers/entities/entities/jcms_flashpoint.lua
@@ -65,7 +65,7 @@ if SERVER then
 		end
 
 		if addedCharge > 0 then
-			local mult = 1 / #team.GetPlayers(1)^(2/3) 
+			local mult = 1 / (#team.GetPlayers(1))^(2/3) 
 			addedCharge = math.ceil( addedCharge * mult )
 			self:SetCharge( math.min(self:GetCharge() + addedCharge, self:GetMaxCharge()) )
 
